028559354db05c89be82c9f72f9a0b221c54f99f
david-aguiyi <davidaguiyi9@gmail.com>
2022-07-17 16:27:34 +0100

    Created js file and linked to html

---
+++ script.js
@@ -0,0 +1,5 @@
+// const plus = document.querySelector("#plus");
+// const minus = document.querySelector("#minus");
+// const counter = document.querySelector(".rcounter");
+
+// minus.addEventListener("e", () => {});
