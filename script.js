b5faa78d44b0958a39dd915344a0bddf967fd05e
david-aguiyi <davidaguiyi9@gmail.com>
2022-07-18 15:01:59 +0100

    Made increment and decrement buttons functional.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-// const plus = document.querySelector("#plus");
-// const minus = document.querySelector("#minus");
-// const counter = document.querySelector(".rcounter");
+const minus = document.querySelector(".minus");
+const counter = document.querySelector(".rcounter");
+const btncount = document.querySelectorAll(".btn-counter");
+let amt = 0;
+function handleAmtBtnClick(e) {
+  if (e.currentTarget.id === "minus") {
+    amt === 0 ? null : amt--;
+  } else {
+    amt++;
+  }
+  counter.textContent = amt;
+}
 
-// minus.addEventListener("e", () => {});
+btncount.forEach((b) => b.addEventListener("click", handleAmtBtnClick));
